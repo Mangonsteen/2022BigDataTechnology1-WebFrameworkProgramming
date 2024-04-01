717f86312776648b046007e0ac7f1995cb55a0c3
彭佳 <486904116@qq.cm>
2024-04-01 22:21:40 +0800

    彭佳的第三次提交

--- src/main/java/com/example/homework03/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.homework03.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.homework03.bean.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
